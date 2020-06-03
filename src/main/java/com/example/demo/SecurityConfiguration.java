0e1691470cef014808e137645d65ea2352327844
rap9210 <rap9210@gmail.com>
2020-06-03 15:39:37 -0400

    SB Security 401

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http)throws Exception{
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema()
+                .withUser(User.withUsername("user")
+                .password(passwordEncoder().encode("user"))
+                        .roles("USER"));
+    }
+
+}
